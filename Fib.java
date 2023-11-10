1c49ceb639863414c4ba0ea56df4957a08dc2a4c
Likhita017 <150414986+Likhita017@users.noreply.github.com>
2023-11-10 10:58:28 +0530

    Update Fib.java

--- Fib.java
+++ Fib.java
@@ -4,7 +4,7 @@ class Fib
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number");
+		System.out.println("Enter a value:@");
 		int n=sc.nextInt();
 		int n1=0,n2=1,n3=0;
 		System.out.println(n1);
